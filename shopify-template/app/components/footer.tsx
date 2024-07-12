704dfee2c39b771de12c62e482a2089f6a5e5a20
Daniel <danielperezf98@gmail.com>
2024-07-12 12:21:09 -0700

    initial commit

---
+++ shopify-template/app/components/footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4 text-center">
+      <div className="container mx-auto">
+        <p>&copy; 2024 Your Company. All rights reserved.</p>
+        <p>Designed by Daniel Perez</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
